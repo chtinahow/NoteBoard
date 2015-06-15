6cff570133aaabf9308ae4657b30a9599bfd5e28
Tina Howard <chh2538@rit.edu>
2015-06-15 10:45:06 -0400

    Added the start of the landscape 2 prototype

---
+++ LandScapeV2/LandScapeV2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LandScapeV2
+//
+//  Created by Tina on 6/15/15.
+//  Copyright (c) 2015 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LandScapeV2/LandScapeV2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LandScapeV2
+//
+//  Created by Tina on 6/15/15.
+//  Copyright (c) 2015 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
