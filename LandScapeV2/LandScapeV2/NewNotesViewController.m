c3228923f8788f9338869ac4301af50aba5b3caf
MukeytheMuse <kimsookoo121@gmail.com>
2015-08-07 16:07:11 -0400

    Pushing again

--- LandScapeV2/LandScapeV2/NewNotesViewController.m
+++ LandScapeV2/LandScapeV2/NewNotesViewController.m
@@ -2,6 +2,7 @@
 //  NewNotesViewController.m
 //  LandScapeV2
 //
+//  A few updates have been made; camera roll to be added
 //  Created by Student on 7/31/15.
 //  Copyright (c) 2015 Student. All rights reserved.
 //
